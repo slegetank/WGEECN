3cb75021cafde21ef54d0b7ec38ba1d7590fa3f4
shuai.zhao <shuai.zhao@linkplay.com>
2018-02-27 13:46:04 +0800

    副总用 -> 副作用

--- html/html.el
+++ html/html.el
@@ -1,16 +1,20 @@
+(defun WGEECN/export-org-to-html (org-name)
+  "Export org file to html for WGEECN."
+  (let ((org-path (format "../%s" org-name)))
+    (with-current-buffer (or (find-buffer-visiting org-name) (find-file org-path))
+      (setq local-enable-local-variables nil)
+      ;; modify relative path
+      (goto-char (point-min))
+      (replace-regexp "file:resource" "file:../resource" )
+      ;; export
+      (let ((file (org-export-output-file-name ".html" nil "html"))
+            (org-export-coding-system org-html-coding-system))
+        (org-export-to-file 'html file))
+      (set-buffer-modified-p nil)
+      (kill-buffer org-name))))
+
 ;;; export all orgs to htmls
 (let ((org-files (directory-files ".." nil "\\.org$")))
     (dolist (org-name org-files nil)
       (message "Export %s ..." org-name)
-      (let ((org-path (format "../%s" org-name)))
-        (with-current-buffer (or (find-buffer-visiting org-name) (find-file org-path))
-          (setq local-enable-local-variables nil)
-          ;; modify relative path
-          (goto-char (point-min))
-          (replace-regexp "file:resource" "file:../resource" )
-          ;; export
-          (let ((file (org-export-output-file-name ".html" nil "html"))
-                 (org-export-coding-system org-html-coding-system))
-            (org-export-to-file 'html file))
-          (set-buffer-modified-p nil)
-          (kill-buffer org-name)))))
+      (WGEECN/export-org-to-html org-name)))
